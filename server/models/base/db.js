9ea26bdcfd84f641fae4727a41bbd3e7427e2f61
wwicaksono <wisnu.wicaksono@detik.com>
2018-07-23 13:26:15 +0700

    add dburl

--- server/models/base/db.js
+++ server/models/base/db.js
@@ -8,6 +8,8 @@ const config = {
   password: process.env.PGPASSWORD,
 };
 
+const dbUrl = process.env.DATABASE_URL;
+
 const pg = pgp({
   connect(client) {
     const cp = client.connectionParameters;
@@ -17,6 +19,6 @@ const pg = pgp({
     const cp = client.connectionParameters;
     console.log('Disconnecting from database:', cp.database);
   },
-})(config);
+})(process.env.NODE_ENV === 'production' ? dbUrl : config);
 
 export default pg;
