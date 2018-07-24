a2da23050b33a12083a840757a44b5f4122c5d07
wwicaksono <wisnu.wicaksono@detik.com>
2018-07-24 14:06:18 +0700

    test fetch

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    fetch('/v1/user/all')
+      .then(response => response.json())
+      .then(data => this.setState({ users: data }));
+  }
   render() {
+    const { users } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,8 +25,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lol
         </p>
+        <ul>
+            {users.map(user =>
+              <li key={user.userid}>
+                <span>{user.username}</span>
+              </li>
+            )}
+          </ul>
       </div>
     );
   }
