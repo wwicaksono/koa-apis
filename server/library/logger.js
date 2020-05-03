389abffee3d12e8d0a45ab1db7e6300dc4f642f0
Wisnu Wicaksono <wisnuwicak2@gmail.com>
2020-05-03 14:22:21 +0700

    Remove pino pretty logger

--- server/library/logger.js
+++ server/library/logger.js
@@ -1,7 +1,6 @@
 import logger from 'koa-pino-logger';
 
 const pinoLogger = logger({
-  prettyPrint: true,
   // extreme: true,
   level: (process.env.NODE_ENV === 'production' ? 'warn' : 'debug'),
 });
